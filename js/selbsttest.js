4262165457f22f766b8cd81240dc69f0d04bb169
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-14 14:42:37 +0100

    Update selbsttest.js

--- js/selbsttest.js
+++ js/selbsttest.js
@@ -53,8 +53,22 @@ function gpGetSource(){
   }
 }
 
+function gpGetUTM(){
+  try{
+    const url = new URL(window.location.href);
+    return {
+      utm_source: url.searchParams.get("utm_source") || "",
+      utm_medium: url.searchParams.get("utm_medium") || "",
+      utm_campaign: url.searchParams.get("utm_campaign") || ""
+    };
+  } catch(e){
+    return {utm_source:"", utm_medium:"", utm_campaign:""};
+  }
+}
+
 function gpSendTestEvent(result, score){
   try {
+    const utm = gpGetUTM();
     fetch(TRACKING_URL, {
       method: "POST",
       body: JSON.stringify({
