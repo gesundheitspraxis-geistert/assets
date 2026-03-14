27412b9323e1de0e59989ca33e787f64d5c9c27f
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-14 21:03:06 +0100

    Update selftest-v11.js

--- js/selftest-v11.js
+++ js/selftest-v11.js
@@ -32,14 +32,6 @@ function gpGetPreviousPath() {
   }
 }
 
-if (!sessionStorage.getItem("entry_page")) {
-  sessionStorage.setItem("entry_page", gpGetPreviousPath() || window.location.pathname);
-}
-
-if (!sessionStorage.getItem("last_page_before_conversion")) {
-  sessionStorage.setItem("last_page_before_conversion", gpGetPreviousPath() || "");
-}
-
 const GP_FORM_GRUEN = "https://401e9539.sibforms.com/serve/MUIFAB7xEJimOTWDIuRru-zsKDUuFXdIorgj7u8slBnxZl654eKfRdvXPl0lZMPi2cXZWbKy4PkmCJ0pXReKo3A1RFAAP1wdVqjCZZnWCwHyz9EJ7X13EVywq06tSJv3yxcKtthv81PdPFNHR7kn04qD3o2PU8gnvzp3EjkYqt7v6iczUWcFrUoIlSZgDT9VtF0sqWSSa_YOgsRsSg==?v=" + GP_FORM_VER;
 
 const GP_FORM_GELB = "https://401e9539.sibforms.com/serve/MUIFABhoIJM37VBTosoxmFVElHBfSLSiE_53ub9w84L-VQsQfMffdqDozvVZPbnnDKprgEOIqSMaXg3OEHwCVcOxz8mcq9wtStO1vgFdpc-9BHrS7fOWFQgWugMYRdv2904s_hbnM-XGza1bZBAJZrDVRte1Wf2gRDdogRcD1L5_EFssxqzbdohDog8UldlJVyCPUAqiWBs7wnJVnQ==?v=" + GP_FORM_VER;
