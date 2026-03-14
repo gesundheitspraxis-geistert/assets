6a2a7881531dff057d2b40142040844a906aed4a
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-14 19:06:42 +0100

    Update selftest-v5.js

--- js/selftest-v5.js
+++ js/selftest-v5.js
@@ -17,6 +17,27 @@ console.log("SELBSTTEST AKTIV 2026-03-14-9");
 
 const TRACKING_URL = "https://script.google.com/macros/s/AKfycbw_2Cc16r6gn39LrGZetEzWzf9cqKTFV5kPlDLbcriYOZ_UgVbX7KiDbXosf9g07y1iUg/exec";
 
+function gpGetPreviousPath() {
+  try {
+    if (!document.referrer) return "";
+
+    const refUrl = new URL(document.referrer);
+    const ownHost = window.location.hostname.replace(/^www\./, "");
+    const refHost = refUrl.hostname.replace(/^www\./, "");
+
+    if (refHost !== ownHost) return "";
+    return refUrl.pathname || "";
+  } catch (e) {
+    return "";
+  }
+}
+
+if (!sessionStorage.getItem("entry_page")) {
+  sessionStorage.setItem("entry_page", gpGetPreviousPath() || window.location.pathname);
+}
+
+sessionStorage.setItem("last_page_before_conversion", gpGetPreviousPath() || "");
+
 const GP_FORM_GRUEN = "https://401e9539.sibforms.com/serve/MUIFAB7xEJimOTWDIuRru-zsKDUuFXdIorgj7u8slBnxZl654eKfRdvXPl0lZMPi2cXZWbKy4PkmCJ0pXReKo3A1RFAAP1wdVqjCZZnWCwHyz9EJ7X13EVywq06tSJv3yxcKtthv81PdPFNHR7kn04qD3o2PU8gnvzp3EjkYqt7v6iczUWcFrUoIlSZgDT9VtF0sqWSSa_YOgsRsSg==?v=" + GP_FORM_VER;
 
 const GP_FORM_GELB = "https://401e9539.sibforms.com/serve/MUIFABhoIJM37VBTosoxmFVElHBfSLSiE_53ub9w84L-VQsQfMffdqDozvVZPbnnDKprgEOIqSMaXg3OEHwCVcOxz8mcq9wtStO1vgFdpc-9BHrS7fOWFQgWugMYRdv2904s_hbnM-XGza1bZBAJZrDVRte1Wf2gRDdogRcD1L5_EFssxqzbdohDog8UldlJVyCPUAqiWBs7wnJVnQ==?v=" + GP_FORM_VER;
