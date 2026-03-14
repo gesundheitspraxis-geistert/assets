85d2e77e2a4994bd8b8dc693ba5ef264f893e071
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-14 20:16:47 +0100

    Update selftest-v8.js

--- js/selftest-v8.js
+++ js/selftest-v8.js
@@ -136,7 +136,7 @@ console.log("Payload wird gesendet", {
       },
       body: JSON.stringify({
         event_id: crypto.randomUUID(),
-      // visitor_id: gpGetVisitorId(),        event_type: "test_completed",
+        event_type: "test_completed",
         timestamp_start: timestampStart,
         timestamp_end: timestampEnd,
         test_duration_seconds: duration,
