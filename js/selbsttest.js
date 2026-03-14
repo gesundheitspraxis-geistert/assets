a0a6fa2e79b0c6d3c78c32313a1266fa7cf80b8a
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-14 15:44:56 +0100

    Update selbsttest.js

--- js/selbsttest.js
+++ js/selbsttest.js
@@ -12,8 +12,8 @@
 
   // ab hier restlicher Code
 
-const GP_FORM_VER = "2026-03-14-3";
-const TRACKING_URL = "https://script.google.com/macros/s/AKfycbysbzvJW0Jb07pfFPXT1SLbohJJYrp8fGQ6bSGkWKaWLfNVWjnGxdDtgWFeOetSlTE/exec";
+const GP_FORM_VER = "2026-03-14-4";
+const TRACKING_URL = "https://script.google.com/macros/s/AKfycbxazboUj4FbJzyyt1E0HxZIGTTWDi6kfXdXZMINZgCIjFmopBOCXJ9gnwVtq_evMdIp/exec";
 
 const GP_FORM_GRUEN = "https://401e9539.sibforms.com/serve/MUIFAB7xEJimOTWDIuRru-zsKDUuFXdIorgj7u8slBnxZl654eKfRdvXPl0lZMPi2cXZWbKy4PkmCJ0pXReKo3A1RFAAP1wdVqjCZZnWCwHyz9EJ7X13EVywq06tSJv3yxcKtthv81PdPFNHR7kn04qD3o2PU8gnvzp3EjkYqt7v6iczUWcFrUoIlSZgDT9VtF0sqWSSa_YOgsRsSg==?v=" + GP_FORM_VER;
 
