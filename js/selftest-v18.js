3dcf072ad6254ad73b0c2bec330c6cf45d24b7cc
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-15 10:28:56 +0100

    Update selftest-v18.js

--- js/selftest-v18.js
+++ js/selftest-v18.js
@@ -12,7 +12,7 @@
 
   // ab hier restlicher Code
 
-const GP_FORM_VER = "2026-03-14-11";
+const GP_FORM_VER = "2026-03-15-1";
 console.log("SELBSTTEST AKTIV 2026-03-14-11");
 
 const TRACKING_URL = "https://script.google.com/macros/s/AKfycbylVzP56vvTMeSXFY6Nt7WspkgL6UMyMmpHaRVcoEZuLby_vPKGuIusMJwnq2sakjrnsw/exec";
@@ -55,11 +55,11 @@ function gpIsTestPage(path) {
   } catch (e) {}
 })();
   
-const GP_FORM_GRUEN = "https://401e9539.sibforms.com/serve/MUIFAB7xEJimOTWDIuRru-zsKDUuFXdIorgj7u8slBnxZl654eKfRdvXPl0lZMPi2cXZWbKy4PkmCJ0pXReKo3A1RFAAP1wdVqjCZZnWCwHyz9EJ7X13EVywq06tSJv3yxcKtthv81PdPFNHR7kn04qD3o2PU8gnvzp3EjkYqt7v6iczUWcFrUoIlSZgDT9VtF0sqWSSa_YOgsRsSg==" + GP_FORM_VER;
+const GP_FORM_GRUEN = "https://401e9539.sibforms.com/serve/MUIFAB7xEJimOTWDIuRru-zsKDUuFXdIorgj7u8slBnxZl654eKfRdvXPl0lZMPi2cXZWbKy4PkmCJ0pXReKo3A1RFAAP1wdVqjCZZnWCwHyz9EJ7X13EVywq06tSJv3yxcKtthv81PdPFNHR7kn04qD3o2PU8gnvzp3EjkYqt7v6iczUWcFrUoIlSZgDT9VtF0sqWSSa_YOgsRsSg==";
 
-const GP_FORM_GELB = "https://401e9539.sibforms.com/serve/MUIFABhoIJM37VBTosoxmFVElHBfSLSiE_53ub9w84L-VQsQfMffdqDozvVZPbnnDKprgEOIqSMaXg3OEHwCVcOxz8mcq9wtStO1vgFdpc-9BHrS7fOWFQgWugMYRdv2904s_hbnM-XGza1bZBAJZrDVRte1Wf2gRDdogRcD1L5_EFssxqzbdohDog8UldlJVyCPUAqiWBs7wnJVnQ==" + GP_FORM_VER;
+const GP_FORM_GELB = "https://401e9539.sibforms.com/serve/MUIFABhoIJM37VBTosoxmFVElHBfSLSiE_53ub9w84L-VQsQfMffdqDozvVZPbnnDKprgEOIqSMaXg3OEHwCVcOxz8mcq9wtStO1vgFdpc-9BHrS7fOWFQgWugMYRdv2904s_hbnM-XGza1bZBAJZrDVRte1Wf2gRDdogRcD1L5_EFssxqzbdohDog8UldlJVyCPUAqiWBs7wnJVnQ==";
 
-const GP_FORM_ROT = "https://401e9539.sibforms.com/serve/MUIFAIXB78-mVd4nt0f0Brwpkd1Gcq-1p8FvVfmVtMWx9iK1uIDEaR_ah0xHQCFTKy08utalxpCnzTLuNcDNSkQlh2HZw_UZMBkrlq5PWODV2XXp1z3pxjwr843Cqhn8WaDq--rVKAItUcesWfmP7czOSMcnmnIh5xkJBRbUOLQtzXH7fQpKe_O7Qg3S8_72R9V5YyBSRxspWxfeSw==" + GP_FORM_VER;
+const GP_FORM_ROT = "https://401e9539.sibforms.com/serve/MUIFAIXB78-mVd4nt0f0Brwpkd1Gcq-1p8FvVfmVtMWx9iK1uIDEaR_ah0xHQCFTKy08utalxpCnzTLuNcDNSkQlh2HZw_UZMBkrlq5PWODV2XXp1z3pxjwr843Cqhn8WaDq--rVKAItUcesWfmP7czOSMcnmnIh5xkJBRbUOLQtzXH7fQpKe_O7Qg3S8_72R9V5YyBSRxspWxfeSw==";
 
 function gpFormUrl(status){
 
