d39be1a2824d68393bdd25231ca6725824b0cb42
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-16 15:00:29 +0100

    Update selbsttest-2026-03-16-v02.js

--- js/selbsttest-2026-03-16-v02.js
+++ js/selbsttest-2026-03-16-v02.js
@@ -19,7 +19,7 @@
 
   // ab hier restlicher Code
 
-const GP_FORM_VER = "2026-03-16-20";
+const GP_FORM_VER = "2026-03-16-21";
 console.log("SELBSTTEST AKTIV", GP_FORM_VER);
 
 var gpTestCompleted = false;
