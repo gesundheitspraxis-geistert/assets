a0d35f4d9b6f40145029f1021da4f7ae119d0150
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-16 10:24:36 +0100

    Update selftest-v26.js

--- js/versionen/selftest-v26.js
+++ js/versionen/selftest-v26.js
@@ -19,7 +19,7 @@
 
   // ab hier restlicher Code
 
-const GP_FORM_VER = "2026-03-16-3";
+const GP_FORM_VER = "2026-03-16-4";
 console.log("SELBSTTEST AKTIV", GP_FORM_VER);
 
 const TRACKING_URL = "https://script.google.com/macros/s/AKfycbyNxqjq_Q3ww-YXtcH4h8GlXccdlDtfAJffxBnkpIMo47b_pinTH_bR5hZB1Qr2MmVp5g/exec";
