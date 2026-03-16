85055e57371b4504ac3c5a92541f3a7b37bf2245
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-16 14:48:00 +0100

    Update selbsttest-2026-03-16-v02.js

--- js/selbsttest-2026-03-16-v02.js
+++ js/selbsttest-2026-03-16-v02.js
@@ -110,16 +110,16 @@ function gpGetUTM(){
       utm_source: url.searchParams.get("utm_source") || sessionStorage.getItem("gp_attr_utm_source") || "",
       utm_medium: url.searchParams.get("utm_medium") || sessionStorage.getItem("gp_attr_utm_medium") || "",
       utm_campaign: url.searchParams.get("utm_campaign") || sessionStorage.getItem("gp_attr_utm_campaign") || "",
-      utm_content: url.searchParams.get("utm_content") || sessionStorage.getItem("gp_attr_utm_content") || ""
-      utm_term: url.searchParams.get("utm_term") || sessionStorage.getItem("gp_attr_utm_term") || "",
-    };
-  } catch(e){
-    return {
-      utm_source: url.searchParams.get("utm_source") || sessionStorage.getItem("gp_attr_utm_source") || "",
-      utm_medium: url.searchParams.get("utm_medium") || sessionStorage.getItem("gp_attr_utm_medium") || "",
-      utm_campaign: url.searchParams.get("utm_campaign") || sessionStorage.getItem("gp_attr_utm_campaign") || "",
       utm_content: url.searchParams.get("utm_content") || sessionStorage.getItem("gp_attr_utm_content") || "",
       utm_term: url.searchParams.get("utm_term") || sessionStorage.getItem("gp_attr_utm_term") || ""
+    };
+  } catch(e){
+   return {
+      utm_source: sessionStorage.getItem("gp_attr_utm_source") || "",
+      utm_medium: sessionStorage.getItem("gp_attr_utm_medium") || "",
+      utm_campaign: sessionStorage.getItem("gp_attr_utm_campaign") || "",
+      utm_content: sessionStorage.getItem("gp_attr_utm_content") || "",
+      utm_term: sessionStorage.getItem("gp_attr_utm_term") || ""
     };
   }
 }
