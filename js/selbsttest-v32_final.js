a77aea70c2c2fef69032069ba5865359505b5873
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-16 12:29:45 +0100

    Update selbsttest-v32_final.js

--- js/selbsttest-v32_final.js
+++ js/selbsttest-v32_final.js
@@ -22,6 +22,8 @@
 const GP_FORM_VER = "2026-03-16-10";
 console.log("SELBSTTEST AKTIV", GP_FORM_VER);
 
+var gpTestCompleted = false;
+
 const TRACKING_URL = "https://script.google.com/macros/s/AKfycbxUdClD2csFRGCuPWi9WqX_5kLm5G2K-2nyZpTrZirnHtGtEHuNPWCVnunwtWRi9S6k7Q/exec";
 
 function gpIsTestPage(path) {
