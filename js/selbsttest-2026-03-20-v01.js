faf49bd6af35edade28bc68047e933410f826dd5
Jana Poczatek <jana.poczatek@gmail.com>
2026-03-20 18:23:50 +0100

    Update and rename xxx to selbsttest-2026-03-20-v01.js

--- js/selbsttest-2026-03-20-v01.js
+++ js/selbsttest-2026-03-20-v01.js
@@ -19,7 +19,7 @@
 
   // ab hier restlicher Code
 
-const GP_FORM_VER = "2026-03-19-4";
+const GP_FORM_VER = "2026-03-20-1";
 console.log("SELBSTTEST AKTIV", GP_FORM_VER);
 
 var gpTestCompleted = false;
